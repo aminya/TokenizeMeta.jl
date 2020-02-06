ebf7138bfb8c1cded5b9afac620ee5970edd0b37
aminya <aminyahyaabadi74@gmail.com>
2020-02-06 01:54:57 -0600

    ttypeof

--- src/Type.jl
+++ src/Type.jl
@@ -34,3 +34,25 @@ macro tisdefined(t)
     return :(tisdefined($m, $t))
 end
 
+################################################################
+"""
+    ttypeof(t)
+    ttypeof(t, check_isdefined::Bool = false)
+
+Returns the type of an evaluated Token
+
+If you set `check_isdefined` to `true`, and `t` is not defined in the scope it returns `UndefToken` instead of throwing an error.
+
+# Examples
+```jldoctest
+julia> t = collect(tokenize("Int64"))
+
+julia> ttypeof(t)
+DataType
+```
+"""
+function ttypeof(modul::Module, t::T, check_isdefined::Bool = false) where {T <: Union{Token, Array{Token}, String, Symbol, Expr}}
+    tevaled = @teval(t, check_isdefined)
+    typeof(tevaled)
+    return
+ end
